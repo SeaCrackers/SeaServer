41e165e546bd0e31fe3246fc96cbcc63e4c89b18
Eliott Jaquier <eliott.jaquier@sysmosoft.com>
2024-02-28 00:00:11 +0100

    add cors for development angular servers

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
-const io = new Server({ /* options */ });
+const io = new Server({
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on("connection", (socket) => {
     console.log('New client connected');

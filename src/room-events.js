a9ec52dedec1dc06400eaf11af870e250fe0a910
Eliott Jaquier <eliott.jaquier@sysmosoft.com>
2024-02-28 12:23:28 +0100

    add room events from Angular TS file + implement simple room state (joining / leave / broadcast)

---
+++ src/room-events.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RoomEvents = void 0;
+var RoomEvents;
+(function (RoomEvents) {
+    RoomEvents["Join"] = "Join";
+    RoomEvents["Leave"] = "Leave";
+    RoomEvents["Broadcast"] = "Broadcast";
+})(RoomEvents || (exports.RoomEvents = RoomEvents = {}));
